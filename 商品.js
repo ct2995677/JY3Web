6a94471437fae8909ed8634717f3e40b6737d779
ct2995677 <48548862+ct2995677@users.noreply.github.com>
2019-03-29 22:59:13 +0800

    Add files via upload

--- 商品.js
+++ 商品.js
@@ -1,13 +1,94 @@
 $(function(){
 				$("#huadong").css({"display":"flex","height":"100%","flex-direction":"column"});
+				
+				$("#huadong").append("<div id='shangpin'></div>");
+				$("#shangpin").css({"width":"100%","display":"flex","background-color":"white","flex-direction":"column"});
+				$("#biaoti").css({"display":"flex","width":"100%","height":"100px","background-color":"#996699","font-size":"71px","font-family":"jokerman","color":"white","justify-content":"center"});
+				$("#shangpin").append("<div id='bannder'></div>");
+				
+				
+				$("#bannder").css({"width":"100%","height":"713px"});
+				
+				
+				$("#shangpin").append("<img src='http://pp0aocw81.bkt.clouddn.com/%E5%BE%AE%E4%BF%A1%E6%88%AA%E5%9B%BE_20190329084842.png' class='sanjiao' />");
+				$(".sanjiao").css({"height":"146px","display":"flex"});
+				$("#shangpin").append("<p id='timu'><b>别住酒店，住我家</b></p>");
+				$("#timu").css({"font-size":"100px","display":"flex","color":"#996699","text-indent":"19px","margin-top":"105px"});
+				$("#shangpin").append("<p id='zhengwen'><b>换个角度看这个城市，万家灯火，五彩斑斓</b></p>");
+				$("#zhengwen").css({"font-size":"45px","display":"flex","color":"#996666","text-indent":"23px","margin-top":"29px"});
+				$("#shangpin").append("<div class='sousuo'></div>");
+				$(".sousuo").append("<form action='' class='parent'></form>");
+				$(".parent").append("<input type='text' class='search' placeholder='搜索'>");
+				$("#shangpin").append("<hr class='xian'>");
+				$(".xian").css({"display":"flex","width":"90%","background-color":"grey","margin-top":"162px","margin-left":"45px"});
+			
+				
+				
+				$("#shangpin").append("<img src='http://pp0aocw81.bkt.clouddn.com/ss.jpg' class='tuone' />");
+				$(".tuone").css({"width":"100%","height":"641px","display":"flex","margin-top":"73px"});
+				$("#shangpin").append("<div id='diyigezi'></div>")
+				$("#diyigezi").css({"display":"flex"});
+				$("#diyigezi").append("<div id='zi1'><p><b>[粉黛•满满少女心•大落地窗江景房]紧<br />邻网红李子坝轻轨穿楼/从你的全世界<br />路过鹅岭二厂/轻轨/拍照美美</b></p></div>");
+				$("#zi1").css({"font-size":"35px","margin-top":"20px","margin-bottom":"20px","flex":"2","padding-left":"35px"});
+				$("#diyigezi").append("<div id='zi11'><p><b>￥174</b></p></div>");
+				$("#zi11").css({"font-size":"50px","margin-top":"20px","margin-bottom":"20px","flex":"1","display":"flex","border-left":"2px solid"});
+				$("#zi11 p").css({"margin":"auto"});
+				
+				
+				
+				$("#shangpin").append("<img src='http://pp0aocw81.bkt.clouddn.com/748e3538-d3eb-43d8-a685-b367ed6d8f58.jpg' class='tutwo' />");
+				$(".tutwo").css({"width":"100%","height":"640px","display":"flex"});
+				$("#shangpin").append("<div id='diergezi'></div>")
+				$("#diergezi").css({"display":"flex"});
+				$("#diergezi").append("<div id='zi2'><p><b>[远方和山]渝中近网红李子坝轻轨,直达<br />磁器口洪崖洞解放碑,全景落地窗+无敌<br />江景+照片打印机,安静宜居</b></p></div>");
+				$("#zi2").css({"font-size":"35px","margin-top":"20px","margin-bottom":"20px","flex":"2","padding-left":"35px"});
+				$("#diergezi").append("<div id='zi22'><p><b>￥129</b></p></div>");
+				$("#zi22").css({"font-size":"50px","margin-top":"20px","margin-bottom":"20px","flex":"1","display":"flex","border-left":"2px solid"});
+				$("#zi22 p").css({"margin":"auto"});
+				
+				
+				
+				$("#shangpin").append("<img src='http://pp0aocw81.bkt.clouddn.com/8d688997-51ac-4a49-9bcf-1fcb0d4c48fb.jpg' class='tuthree' />");
+				$(".tuthree").css({"width":"100%","height":"586px","display":"flex"});
+				$("#shangpin").append("<div id='disangezi'></div>")
+				$("#disangezi").css({"display":"flex"});
+				$("#disangezi").append("<div id='zi3'><p><b>梦幻之厦ins风主题房中山路美食街旁<br  />近鼓浪屿轮渡厦鼓码头厦门大学曾厝</b></p></div>");
+				$("#zi3").css({"font-size":"35px","margin-top":"20px","margin-bottom":"20px","flex":"2","padding-left":"35px"});
+				$("#disangezi").append("<div id='zi33'><p><b>￥268</b></p></div>");
+				$("#zi33").css({"font-size":"50px","margin-top":"20px","margin-bottom":"20px","flex":"1","display":"flex","border-left":"2px solid"});
+				$("#zi33 p").css({"margin":"auto"});
+				
+				
+				
+				
+				$("#shangpin").append("<img src='http://pp0aocw81.bkt.clouddn.com/260a3297-e3b1-49d1-ac7a-7478009b3aca.jpg' class='tufour' />");
+				$(".tufour").css({"width":"100%","height":"640px","display":"flex"});
+				$("#shangpin").append("<div id='disigezi'></div>")
+				$("#disigezi").css({"display":"flex"});
+				$("#disigezi").append("<div id='zi4'><p><b>留白house| Room08<黛>Loft复式双<br />床投影房/BRT沿线直达机场动车站中<br />山路鼓浪屿</b></p></div>");
+				$("#zi4").css({"font-size":"35px","margin-top":"20px","margin-bottom":"20px","flex":"2","padding-left":"35px"});
+				$("#disigezi").append("<div id='zi44'><p><b>￥342</b></p></div>");
+				$("#zi44").css({"font-size":"50px","margin-top":"20px","margin-bottom":"20px","flex":"1","display":"flex","border-left":"2px solid"});
+				$("#zi44 p").css({"margin":"auto"});
+				
+				
+				$("#shangpin").append("<br />");
+				$("#shangpin").append("<br />");
+				$("#shangpin").append("<br />");
+				$("#shangpin").append("<br />");
+				
+				
+				
+				
+				
 				$("#huadong").append("<div id='menu'></div>");
 				$("#menu").css({"height":"100px","background-color":"white","width":"100%","position":"fixed","bottom":"0","display":"flex","flex-direction":"row","align-items":"center"});
 				$("#menu").append("<img src='http://pp0aocw81.bkt.clouddn.com/%E5%A4%B4%E5%83%8F5%20%281%29.png' class='me'>");
 				$(".me").css({"height":"80px","display":"flex","flex":"0.08"});
 				$("#menu").append("<select><option value='beijin'>北京</option><option value='tianjin'>天津</option><option value='shanghai'>上海</option><option value='guangzhou'>广州</option><option value='chengdu'>成都</option><option value='nanjing'>南京</option><option value='shenzhen'>深圳</option><option value='qingdao'>青岛</option></select>");
 				$("select").css({"height":"80px","font-size":"40px","font-weight":"bold","display":"flex","flex":"0.25","padding":"0 2%","text-align":"center","border-top":"none","border-right":"none","border-bottom":"none","text-indent":"69px"});
-				$("option").css({"font-size":"14px","color":"orange","font-weight":"bold"});
-				$("#menu").append("<input type='datetime-local' class='time'/>");
+				$("option").css({"font-size":"14px","color":"rgb(153,102,153)","font-weight":"bold"});
+				$("#menu").append("<input type='date' class='time'/>");
 				$(".time").css({"height":"76px","font-size":"40px","font-weight":"bold","display":"flex","flex":"0.57","border-top":"none","text-indent":"19px","border-bottom":"none"});
 				$("#menu").append("<img src='http://pp0aocw81.bkt.clouddn.com/%E7%AE%AD%E5%A4%B4.png' class='jiantou'>");
 				$(".jiantou").css({"height":"80px","display":"flex","flex":"0.1"});
@@ -19,18 +100,13 @@ $(function(){
 				
 				
 				
-				$("#huadong").append("<div id='shangpin'></div>");
-				$("#shangpin").css({"width":"100%","display":"flex","background-color":"blue","top":"0","flex-direction":"column"});
-//				$("#shangpin").append("<div id='biaoti'></div>");
 				
-				$("#biaoti").css({"width":"100%","height":"100px","background-color":"#996699","font-size":"71px","font-family":"jokerman","color":"white","text-align":"center"});
-				$("#shangpin").append("<div id='bannder'></div>");
 				
-				$("#bannder").css({"width":"100%"});
-
-
-
-
-
-
+				$("#huadong").append("<div id='gerenxinxi'></div>");
+				$("#gerenxinxi").css({"width":"100%","height":"100%","position":"absolute","left":"-1000px","background-color":"rgba(0,0,0,0.5)"});
+				$("#gerenxinxi").append("<div id='gerencaidan'></div>");
+				$("#gerencaidan").append("<ul><a href='#'><li>我的信息</li></a><a href='#'><li>我的订单</li></a><a href='#'><li>我的收藏</li></a><a href='#'><li>我的钱包</li></a><a href='#'><li>消息</li></a><a href='#'><li>设置</li></a><a href='#'><li>退出</li></a></ul>");
+//				$("#tanchuang").append("<hr /> class='leftxian'>");
+//				$(".leftxian").css({"position":"absolute","left":"100px","top":"100px","color":"red"});
+				
 			})
